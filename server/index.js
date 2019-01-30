ebb7bfb2101c3e52679778262641591da9655fe9
GhaidaAt95 <ghaida.alatoum2@gmail.com>
2019-01-30 11:26:29 -0600

    First Commit

---
+++ server/index.js
@@ -0,0 +1,83 @@
+const keys = require('./keys');
+
+// Express App Setup
+// Require the Express Library
+const express = require('express')
+// Require Body Parses
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Create a new express application
+// This the object that is going to recive and respond
+// to any http requests that are coming or going back 
+// to the re-act application
+const app = express();
+// Wire up cors: cross origin resourse sharing
+// Going to allow us to make requests from one domain
+// that the react application is going to be running on 
+// to a completely different domain or different port
+// that the express API is hosted on
+app.use(cors());
+// Parse incoming requests from the react application and turn the body of the post into
+// a json value that our express API can then very easily work with
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+  .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
